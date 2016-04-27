89db80468e7b2ebe7a45af5e74d1e808e82cf1e5
ratanak <ratanak17@gmail.com>
2016-04-27 16:23:06 +0700

    fix sidebar

--- cpanel/client/app/startup/material_init.js
+++ cpanel/client/app/startup/material_init.js
@@ -1,19 +1,16 @@
-////FlowRouter.wait();
-
 Meteor.startup(function () {
-  // $('body').addClass('sidebar-mini fixed');
-//    /*** Material Init For All Template ***/
-//    for (var property in Template) {
-//        // check if the property is actually a blaze template
-//        if (Blaze.isTemplate(Template[property])) {
-//            var template = Template[property];
-//            // assign the template an onRendered callback who simply prints the view name
-//            template.onRendered(function () {
-//                $.material.init();
-//                //console.log('$.material.init() for ' + this.view.name);
-//            });
-//        }
-//    }
-//
+   /*** Material Init For All Template ***/
+   for (var property in Template) {
+       // check if the property is actually a blaze template
+       if (Blaze.isTemplate(Template[property])) {
+           var template = Template[property];
+           // assign the template an onRendered callback who simply prints the view name
+           template.onRendered(function () {
+               $.material.init();
+               //console.log('$.material.init() for ' + this.view.name);
+           });
+       }
+   }
+
 //    //FlowRouter.initialize();
 });
