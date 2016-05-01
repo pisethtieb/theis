365c0881879c1e37ee585ec2161e79b1c56997e0
pisethtieb <pisethsomaly@hotmail.com>
2016-05-01 10:19:24 +0700

    fixed contract tarbula

--- rabbit/common/tabulars/contract.js
+++ rabbit/common/tabulars/contract.js
@@ -19,16 +19,18 @@ Rabbit.TabularTable.Contract = new Tabular.Table({
             }
         },
         {data: "_product.name", title: "Product"},
-          {data: "productType", title: "Type"},
+        {data: "productType", title: "Type"},
         {
             data: "_officeCount",
             title: "Office <i class='fa fa-arrow-up'></i>",
             tmpl: Meteor.isClient && Template.rabbit_officeLinkAction
-        }, {
-            data: "_paymentOfficeCount",
-            title: "Pay Office <i class='fa fa-arrow-up'></i>",
-            tmpl: Meteor.isClient && Template.rabbit_paymentOfficeLinkAction
-        }, {
+        },
+        // {
+        //     data: "_paymentOfficeCount",
+        //     title: "Pay Office <i class='fa fa-arrow-up'></i>",
+        //     tmpl: Meteor.isClient && Template.rabbit_paymentOfficeLinkAction
+        // },
+        {
             data: "_paymentMaintenanceCount",
             title: "Pay Maintenance <i class='fa fa-arrow-up'></i>",
             tmpl: Meteor.isClient && Template.rabbit_paymentMaintenanceLinkAction
@@ -38,5 +40,5 @@ Rabbit.TabularTable.Contract = new Tabular.Table({
             tmpl: Meteor.isClient && Template.rabbit_addFile
         }
     ],
-    extraFields: ['customerId', 'contractDate', 'basePrice', 'maintenancePrice', 'paymentMethod', 'type', 'testing', 'maintenanceFee', 'des', '_product', '_customer', 'addFile', 'contractorId', 'agentId', 'paymentMethod', 'amount', 'productId', 'monthlyFee', 'installationFee', 'trainingFee', 'productType']
+    extraFields: ['customerId', 'contractDate', 'basePrice', 'maintenancePrice', 'paymentMethod', 'type', 'testing', 'maintenanceFee', 'des', '_product', '_customer', 'addFile', 'contractorId', 'agentId', 'paymentMethod', 'amount', 'productId', 'monthlyFee', 'installationFee', 'trainingFee', 'productType', "_paymentOfficeCount"]
 });
