96c0ea6837518fdfb765af9e9d45dbb4f11942d7
pisethtieb <pisethsomaly@hotmai.com>
2016-06-25 20:37:38 +0700

    fixed quotaiton report

--- rabbit/client/app/subscriptions/global.js
+++ rabbit/client/app/subscriptions/global.js
@@ -1,4 +1,4 @@
-//Meteor.subscribe('rabbit_product');
+Meteor.subscribe('rabbit_quotation');
 //Meteor.subscribe('rabbit_customer');
 Meteor.subscribe('rabbit_contract');
 ////Meteor.subscribe('rabbit_office');
